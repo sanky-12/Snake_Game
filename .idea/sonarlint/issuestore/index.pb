a92ff19ab8d219c4014046e1bf306326b191b7a6
Sanket Dumbare <sanketdumbare.sd@gmail.com>
2024-09-19 12:13:52 -0700

    Snake Class defination

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,12 @@
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 S
 #src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+S
+#src/main/java/org/example/Game.java,f\1\f1d67a7ee35efa3e1601241987bec47697a781ba
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+T
+$src/main/java/org/example/Snake.java,8\f\8ff59958c9b407f7b3914386f76bc50765e5f5b2
+S
+#src/main/java/org/example/Food.java,1\9\190b40a772bf2e08319a2cd0d0058178b178ade0
---
+++ src/main/java/org/example/Snake.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.awt.event.KeyEvent;
+
+public class Snake {
+    private ArrayList<Point> body;
+    private int unitSize;
+
+    public Snake(int startX, int startY, int unitSize){
+        this.unitSize = unitSize;
+        body = new ArrayList<>();
+        body.add(new Point(startX, startY));
+    }
+
+    public void move(int direction){
+        Point head = new Point(body.get(0));
+
+        switch (direction){
+            case KeyEvent.VK_UP:
+                head.y -= unitSize;
+                break;
+            case KeyEvent.VK_DOWN:
+                head.y += unitSize;
+                break;
+            case KeyEvent.VK_LEFT:
+                head.x -= unitSize;
+                break;
+            case KeyEvent.VK_RIGHT:
+                head.x += unitSize;
+        }
+        body.add(0,head);
+        body.remove(body.size()-1);
+    }
+
+    public void grow(){
+        body.add(new Point(body.get(body.size()-1)));
+    }
+
+    public Point getHead(){
+        return body.get(0);
+    }
+
+    public boolean hasCollision(int width, int height){
+        Point head = getHead();
+        //Check ofr hte borders
+        if(head.x < 0 || head.x > width || head.y < 0 || head.y> height){
+            return true;
+        }
+
+        //Check for itself collision
+        for (Point point : body) {
+            if (head.equals(point)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void draw(Graphics g){
+        g.setColor(Color.GREEN);
+        for (Point point : body){
+            g.fillRect(point.x, point.y, unitSize, unitSize);
+        }
+    }
+}
